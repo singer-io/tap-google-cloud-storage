a101df467a84e98563bfc9c6dcb14310e88b0b65
Mukesh Bhatt <mukesh.bhatt@qlik.com>
2026-01-16 18:14:13 +0530

    initial commit for testcases (#3)
    * initial commit for testcases
    
    * remove unwanted files
    
    * update testcases
    
    * Integration tests gcs (#4)
    
    * initial discovery test
    
    * added few integration test and config
    
    * added tests
    
    * integration test for zip
    
    * update test for bookmark

---
+++ tests/test_gz_file_contains_csv_file.py
@@ -0,0 +1,45 @@
+from tap_tester import connections, menagerie, runner
+from base import GCSBaseTest
+from utils_for_test import delete_and_push_file
+
+
+class GCSCompressedGZFileCSV(GCSBaseTest):
+
+    table_entry = [{'table_name': 'gz_has_csv_data', 'search_prefix': 'tap_gcs_tester/gz_csv', 'search_pattern': 'tap_gcs_tester/gz_csv/.*\\.gz', 'key_properties': []}]
+
+    def setUp(self):
+        delete_and_push_file(self.get_properties(), self.resource_names(), None)
+        self.conn_id = connections.ensure_connection(self)
+
+    def resource_names(self):
+        return ["sample_compressed_gz_file.gz"]
+
+    def name(self):
+        return "test_gz_file_contains_csv_file"
+
+    def expected_check_streams(self):
+        return {'gz_has_csv_data'}
+
+    def expected_sync_streams(self):
+        return {'gz_has_csv_data'}
+
+    def expected_pks(self):
+        return {'gz_has_csv_data': {}}
+
+    def test_run(self):
+        found_catalogs = self.run_and_verify_check_mode(self.conn_id)
+
+        # Clear state before our run
+        menagerie.set_state(self.conn_id, {})
+
+        our_catalogs = [c for c in found_catalogs if c.get('tap_stream_id') in self.expected_sync_streams()]
+
+        self.perform_and_verify_table_and_field_selection(self.conn_id, our_catalogs)
+
+        self.run_and_verify_sync(self.conn_id)
+
+        expected_records = 998
+        # Verify actual rows were synced
+        records = runner.get_upserts_from_target_output()
+
+        self.assertEqual(expected_records, len(records))
