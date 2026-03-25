fd316094a246bd9caec60467ee3ae1576f6682dc
mukeshbhatt18gl <mukesh.bhatt@qlik.com>
2026-03-25 16:26:44 +0530

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-google-cloud-storage',
-      version='0.0.2',
+      version='0.0.1',
       description='Singer tap for extracting files from Google Cloud Storage',
       author='Stitch',
       url='https://singer.io',
